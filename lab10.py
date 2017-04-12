eee8ef66d86a7a0d7fa8ef0659e1f8a85bad3b56
bcvancouver <michael-xi@hotmail.com>
2017-04-11 22:26:08 -0600

    somewhat working but result is terrible

--- lab10.py
+++ lab10.py
@@ -87,17 +87,14 @@ def initTracker(img, corners):
     # setup initial location of window
     #r, h, c, w = 250, 90, 400, 125  # simply hardcoded the values
 
-    #print corners
     getCorners(corners)
     global track_window
     w = urx - ulx
     h = lly - uly
     track_window = (ulx, uly, w, h)
-    #print track_window
     # set up the ROI for tracking
-    roi = img[ulx: urx][uly: lry]
-    #roi = img
-    hsv_roi = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    roi = img[ulx: urx, uly: lry]
+    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
     global roi_hist
     roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
@@ -106,6 +103,7 @@ def initTracker(img, corners):
     # Setup the termination criteria, either 10 iteration or move by at least 1 pt
     global term_crit
     term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
     return
 
 
@@ -114,13 +112,15 @@ def updateTracker(img):
     # at present it simply returns the actual corners with an offset so that
     # a valid value is returned for the code to run without errors
     # this is only for demonstration purpose and your code must NOT use actual corners in any way
+    #print roi
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    global roi_hist
+
     dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
     # apply meanshift to get the new location
     global track_window, term_crit
     ret, track_window = cv2.CamShift(dst, track_window, term_crit)
     actual_corners = updateCorners(track_window)
+    print track_window
     return actual_corners
 
 
